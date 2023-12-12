dfb1efa26b6dbe277638d4916b886a926c1a94f5
Nieko123 <115734414+Nieko123@users.noreply.github.com>
2023-12-12 14:09:19 -0500

    v0.1

--- 06_OOP/oop.py
+++ 06_OOP/oop.py
@@ -1,4 +1,4 @@
-# Object- Oriented Programing, Nieko Garnes, v0.0
+# Object- Oriented Programing, Nieko Garnes, v0.1
 
 class Person: # Use PascalCase
     def __init__(self, name, age, weight): 
@@ -8,10 +8,24 @@ class Person: # Use PascalCase
 
     # To String Function
     def __str__(self):
-        return f"Name: {self.name}\nThis person is {self.age} years old.\nThey weigh {self.weigh} pounds\n"
+        return f"Name: {self.name}\nThis person is {self.age} years old.\nThey weigh {self.weight} pounds\n"
 
+    def classFunction():
+        print("This is an example class function.\n")
+        print("It only works when called on an object of that class.")
 
-
-person1 = Person("George Washington", 13, 255.5)
-
+person1 = Person("George Washington", 45, 255.5)
+person2 = Person("John F Kennedy", 52, 167.5)
 print(person1)
+print(person2)
+
+if person1_weight > person2.weight:
+    print(f"{person1.name} weighs more than {person2.name}.")
+elif person1.weight == person2.weight:
+    print("Each person weighs the same.\n")
+else:
+    print(f"{person2.name} weighs more than {person1.name}.")
+
+person1.classFunction()
+
+
