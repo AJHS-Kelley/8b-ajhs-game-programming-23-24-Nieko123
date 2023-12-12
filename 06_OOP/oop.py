f2af13091ec8a00fc1a8530200b983d13e5c54b8
Nieko123 <115734414+Nieko123@users.noreply.github.com>
2023-12-12 13:53:45 -0500

    v0.0

--- 06_OOP/oop.py
+++ 06_OOP/oop.py
@@ -1,3 +1,17 @@
 # Object- Oriented Programing, Nieko Garnes, v0.0
 
 class Person: # Use PascalCase
+    def __init__(self, name, age, weight): 
+        self.name = name
+        self.age = age
+        self.weight = weight
+
+    # To String Function
+    def __str__(self):
+        return f"Name: {self.name}\nThis person is {self.age} years old.\nThey weigh {self.weigh} pounds\n"
+
+
+
+person1 = Person("George Washington", 13, 255.5)
+
+print(person1)
