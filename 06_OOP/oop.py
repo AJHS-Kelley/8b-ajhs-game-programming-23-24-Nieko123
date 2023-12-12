b4015a03c0247bce6fd219d947133f206f60a582
Nieko123 <115734414+Nieko123@users.noreply.github.com>
2023-12-12 14:26:38 -0500

    v0.2

--- 06_OOP/oop.py
+++ 06_OOP/oop.py
@@ -16,16 +16,34 @@ class Person: # Use PascalCase
 
 person1 = Person("George Washington", 45, 255.5)
 person2 = Person("John F Kennedy", 52, 167.5)
-print(person1)
+# print(person1)
+# print(person2)
+
+# if person1_weight > person2.weight:
+#     print(f"{person1.name} weighs more than {person2.name}.")
+# elif person1.weight == person2.weight:
+#     print("Each person weighs the same.\n")
+# else:
+#     print(f"{person2.name} weighs more than {person1.name}.")
+
+# person1.classFunction()
+
+# Changing Properties after creation
+print(person1.name)
+person1.name = "George Washington"
+print(person1.name)
+
+# Deleting Properties -- Danger
+# This is does not 'reset' a property, it deletes
+print(person1.name)
+del person1.name
+# print(person1)
+
+# Dleting Objects -- Delete them
+del person1
+
+# Adding Properties to objects
+person2.weakness = "Kryptonite"
 print(person2)
-
-if person1_weight > person2.weight:
-    print(f"{person1.name} weighs more than {person2.name}.")
-elif person1.weight == person2.weight:
-    print("Each person weighs the same.\n")
-else:
-    print(f"{person2.name} weighs more than {person1.name}.")
-
-person1.classFunction()
-
+print(person2.weakness)
 
