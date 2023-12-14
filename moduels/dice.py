6c4ebc9b3f9bd63e93e94ff1d2e543e4734ae727
Nieko123 <115734414+Nieko123@users.noreply.github.com>
2023-12-14 13:40:46 -0500

    v0.1

--- moduels/dice.py
+++ moduels/dice.py
@@ -1,11 +1,25 @@
 # Dice Rolling module by Nieko Garnes, v0.0
 import random
 
-def rollDice(numDice, sizeDice):
+def roll(numDice, sizeDice):
     numRolled = 0
+    sum += roll
     while numRolled < numDice:
         roll = random.randint(1, sizeDice)
         sum += roll
         print(f"Roll: {roll}\n")
+        print(f'Sum: {sum}\n')
         numRolled += 1
     return sum 
+
+def display(numDice, sizeDice):
+    numRolled = 0
+    sum += roll
+    while numRolled < numDice:
+        roll = random.randint(1, sizeDice)
+        sum += roll
+        print(f"Roll: {roll}\n")
+        print(f'Sum: {sum}\n')
+        numRolled += 1
+    return sum 
+
