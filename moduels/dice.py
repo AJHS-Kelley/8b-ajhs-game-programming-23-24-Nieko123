314769d8c2500497d9dd0d32137310854d6b3bd0
Nieko123 <115734414+Nieko123@users.noreply.github.com>
2023-12-14 14:13:58 -0500

    v0.0

--- moduels/dice.py
+++ moduels/dice.py
@@ -1,19 +1,18 @@
 # Dice Rolling module by Nieko Garnes, v0.0
 import random
 
-def roll(numDice, sizeDice):
+def roll(numDice, sizeDice): # Verified Working 12-14-23
     numRolled = 0
     sum += roll
     while numRolled < numDice:
         roll = random.randint(1, sizeDice)
         sum += roll
-        print(f"Roll: {roll}\n")
-        print(f'Sum: {sum}\n')
         numRolled += 1
     return sum 
 
 def display(numDice, sizeDice):
-    numRolled = 0
+    sum = 0
+    roll = 0
     sum += roll
     while numRolled < numDice:
         roll = random.randint(1, sizeDice)
@@ -23,3 +22,16 @@ def display(numDice, sizeDice):
         numRolled += 1
     return sum 
 
+def isDoubles(roll1, roll2):
+    if roll1 == roll2:
+        osDoubles = True
+    else:
+        isDoubles = False
+    return isDoubles
+
+def isExploding(roll, sizeRoll):
+    if roll == sizeRoll:
+        isExploding = True
+    else:
+        isExploding = False
+    return isExploding
