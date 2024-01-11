d278360e56b1d0a506c30c6c30ec892974c42e6b
Nieko123 <115734414+Nieko123@users.noreply.github.com>
2024-01-11 14:00:38 -0500

    v0.1

---
+++ cohortleson/dnaGames/dnaGame.py
@@ -0,0 +1,8 @@
+# DNA Replicated, Nieko Garnes v0.0
+
+# Import Entire Modules -- Get the whole tool box.
+import time, datetime
+
+# Import Specific Methods -- Get the Specific tool.
+from random import choice
+
