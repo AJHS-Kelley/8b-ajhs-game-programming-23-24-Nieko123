681c9a1c111008a89490d15b96e13a1c32b8d6c5
Nieko123 <115734414+Nieko123@users.noreply.github.com>
2024-01-11 14:22:17 -0500

    v0.2

--- cohortleson/dnaGames/dnaGame.py
+++ cohortleson/dnaGames/dnaGame.py
@@ -6,3 +6,21 @@ import time, datetime
 # Import Specific Methods -- Get the Specific tool.
 from random import choice
 
+# Store the DNA Bases
+dnaBases = ["A", "T", "G", "C"]
+
+# Game Fuctions
+def gameIntro() -> None:
+    pass
+
+def genDNA() -> str:
+    bassesGenerated = 0
+    bassesRequested =int( input("Please Enter a positive interger number of bases to generate.\n"))
+    dnaSequence = ""
+
+    while basesGenerated < basesRequested:
+        dnaSequence += choice(dnaBases)
+        dnaGenerated ==1
+    return dnaSequence
+dna = genDNA()
+print(dna)
