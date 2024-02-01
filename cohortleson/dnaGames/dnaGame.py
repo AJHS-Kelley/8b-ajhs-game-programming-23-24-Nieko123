041fc409c601e1be2f6d2bd6e65c0f69a7620766
Nieko123 <115734414+Nieko123@users.noreply.github.com>
2024-02-01 13:56:43 -0500

    v0.9

--- cohortleson/dnaGames/dnaGame.py
+++ cohortleson/dnaGames/dnaGame.py
@@ -1,58 +1,58 @@
-# DNA Replicated, Nieko Garnes v0.0
+#DNA Replication Game, Nieko Garnes, v0.1
 
 # Import Entire Modules -- Get the whole tool box.
-import time, datetime
+import time, datetime 
 
-# Import Specific Methods -- Get the Specific tool.
+# Import Specific Methods -- Get the specific tool.
 from random import choice
 
 # Store the DNA Bases
 dnaBases = ["A", "T", "G", "C"]
 
-# Game Fuctions
+# GAME FUNCTIONS
 def gameIntro() -> None:
     pass
 
 def genDNA() -> str:
     basesGenerated = 0
-    basesRequested =int( input("Please Enter a positive interger number of bases to generate.\n"))
+    basesRequested = int(input("Please enter a positive integer number of bases to generate.\n"))
     dnaSequence = ""
+
     while basesGenerated < basesRequested:
         dnaSequence += choice(dnaBases)
         basesGenerated += 1
     return dnaSequence
 
-dna = genDNA()
-
 def doTranscription(dnaSequence: str) -> tuple:
-    print(f"The DNA sequenceis {dnaSequence}.\n")
-    print("Youwill now generate the RNA sequence that would match.\n")
-    print("Please remember, in the RNA sequence U pairs with A from the DNA sequence.\n")
-    rnaStart = time.time() # time.time() returns the number of seconds since 00:00:00 jan. 01, 1970
-    rnaSequence = input("Please enter the matching RNA sequence. Leave no spaces! Then press enter\n").upper()
+    
+    print(f"The DNA Sequence is {dnaSequence}.\n")
+    print("You will now generate the RNA sequence that would match.\n")
+    print("Please remember, in the RNA sequence U pairs with A from the DNA sequence.")
+    rnaStart = time.time() #time.time() returns the number of seconds since 00:00:00 UTC Jan. 01, 1970
+    rnaSequence = input("Please enter the matching RNA sequence. Leave no spaces! Then press enter.\n").upper()
     rnaStop = time.time()
     rnaTime = rnaStop - rnaStart
-    return(rnaSequence, rnaTime)
-    # tuples are ordered -- you can reference elements with the index.
-    # tuples are UNCHANGEABLE -- you cannot add, modify, or delte after creating
-    # tuples can have duplicate values.
+    return (rnaSequence, rnaTime)
+    # Tuples are ORDERED -- you can reference elements with the index.
+    # Tuples are UNCHANGEABLE -- you cannot add, modify, or delete after creating
+    # Tuples CAN have duplicate values.
 
 def verifySequence(dnaSequence: str, rnaSequence: str) -> bool:
     isMatch = False
     if len(dnaSequence) != len(rnaSequence):
-        print("The sequences are different lengths and cannot match.\n")
+        print("The sequences are diffrent lengths and cannot match.\n")
         return isMatch
-    for dnaBase, rnaBase in(dnaSequence, rnaSequence):
+    for dnaBase, rnaBase in zip(dnaSequence, rnaSequence):
         if dnaBase == "A" and rnaBase == "U":
             isMatch = True
-        elif dnaBase == "C" and rnaBase == "C":
+        elif dnaBase == "C" and rnaBase == "G":
             isMatch = True
-        elif dnaBase == "G" and rnaase == "C":
+        elif dnaBase == "G" and rnaBase == "C":
             isMatch = True
-        elif dnaBase == "T" and rnaBase == "A": 
+        elif dnaBase == "T" and rnaBase == "A":
             isMatch = True
         else:
-            print ("Unable to identify correct base so no match.\n")
+            print ("error message about no match.\n")
     return isMatch
 
 def calcScore(rnaSequence: str, rnaTime: float) -> int:
@@ -63,40 +63,50 @@ def calcScore(rnaSequence: str, rnaTime: float) -> int:
         score += 900000
     elif rnaTime < 15.0:
         score += 700000
-    else: # slowest time
+    elif rnaTime < 25.0:
+        score += 500000
+    else: # Slowest Time, Lowest Score
         score += 250000
 
     scoreMulti = 0.0
-    if len(rnaSequence) > 30:
+    if len(rnaSequence) >+ 30: #Long
+        scoreMulti = 5.0
+    elif len(rnaSequence) >= 25: # Long
+        scoreMulti = 4.0
+    if len(rnaSequence) >+ 20: #Long
+        scoreMulti = 3.0
+    if len(rnaSequence) >+ 15: #Long
         scoreMulti = 5.0
     elif len(rnaSequence) >= 5:
         scoreMulti = 1.0
-    else:
+    else: #Short
         scoreMulti = 0.5
-    score *= scoreMulti 
+    # Increase
+    # Decrease
+    score *= scoreMulti
     return score
 
-def saveScore(dnaSequence: str, rnaSequence: str, rnaTime: float) -> None:
+def saveScore(dnaSequence: str, rnaSequence: str, rnaTime: float, score: int) -> None:
     playerName = input("What is your first name?\n")
-    lastName = input("What is your first name?\n")
-    fullName = playerName + " " + ".txt"
-    fileName = "dnaRep"
-    saveData = open(fileName, a)
+    lastName = input("What is your lastname?\n")
+    fullName = playerName + " " + lastName
 
-    # File Modes
-    #"x mode -- Create File, if file exists, exit with error"
+    fileName= "dnaReplication" + fullName + ".txt"
+    saveData = open(fileName, "a")
+    #File Modes
+    # "x" mode -- CREATE FILE, IF FILE EXISTS, EXIT WITH ERROR
     # "w" mode -- CREATE FILE, IF FILE EXISTS, OVERWRITE IT
-    # "a" mode -- Create FILE, FILE EXISTS, APPEND TO IT
+    # "a" MODE -- CREATE FILE, IF FILE EXISTS, APPEND TO IT
     saveData.write(f"DNA Sequence: {dnaSequence}\nRNA Sequence: {rnaSequence}\n")
     saveData.write(f"Transcription Time: {rnaTime}\n")
     saveData.write(f"Score: {score}\n")
     saveData.write(f"{fullName}\n")
-    saveData.write(f"{datetime.datetime.now()}\n")
-    saveData.close()    
+    saveData.write(f"{datetime.datetime.now()}")
+    saveData.close()
+
 
 dna = genDNA()
 rna = doTranscription(dna)
-print(verifySequence(dna, rna[0]))
-
-print(calcScore(rna[0], rna[1]))
-
+if verifySequence(dna, rna[0]):
+    score = (calcScore(rna[0], rna[1]))
+    saveScore(dna, rna[0], rna[1], score)
