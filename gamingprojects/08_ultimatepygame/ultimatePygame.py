adc7e83f7aef345176f1cf565f1c9b8c42714f58
Nieko123 <115734414+Nieko123@users.noreply.github.com>
2024-02-13 14:44:52 -0500

    v0.5

--- gamingprojects/08_ultimatepygame/ultimatePygame.py
+++ gamingprojects/08_ultimatepygame/ultimatePygame.py
@@ -7,17 +7,19 @@ pygame.init()
 screen = pygame.display.set_mode((800,400))
 pygame.display.set_caption('Runner')
 clock = pygame.time.Clock()
-test_font = pygame.font.Font('None/Pixeltype.ttf', 50)
+test_font = pygame.font.Font(None, 50)
 
-sky_surface = pygame.image.load(('img/ultPie/Sky.jpeg')).convert()
-ground_surface = pygame.image.load('img/ultPie/ground.jfif').convert()
-text_surface = test_font.render('My game', False, 'Green')
+sky_surf = pygame.image.load(('img/ultPie/sky.jpg')).convert()
+ground_surf = pygame.image.load('img/ultPie/ground.jpg').convert()
 
-snail_surface = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
-snail_x_pos = 600
+score_surf = test_font.render('My game', False, (64,64,64))
+score_rect = score_surf.get_rect(center = (400, 50))
 
-player_surf = pygame.image.load('graphics/player/player_walk_1.png').convert_alpha()
-player_rect = player_surf.get_rect(midbottom =(80,300))
+snail_surf = pygame.image.load('img/ultPie/snail.png').convert_alpha()
+snail_rect = snail_surf.get_rect(midbottom = (80,300))
+
+player_surf = pygame.image.load('img/ultPie/player_walk.png').convert_alpha()
+player_rect = player_surf.get_rect(midbottom =(0,0))
 
 while True:
     for event in pygame.event.get():
@@ -27,12 +29,15 @@ while True:
         # if event.type == pygame.MOUSEMOTTON:
         #     if player_rect.collidepoint(event.pos): print('collision')
 
-    screen.blit(sky_surface,(0.0))
-    screen.blit(ground_surface,(0,300))
-    screen.blit(text_surface,(300,50))
-    snail_x_pos -= 4
-    if snail_x_pos < -100: 
-        snail_x_pos = 800
-        screen.blit(text_surface,(snail_x_pos,250))
+    screen.blit(sky_surf,(300,50))
+    screen.blit(ground_surf,(0,300))
+    pygame.draw.rect(screen,'Pink', score_rect)
+    pygame.draw.rect(screen,'Pink', score_rect,10)
+    screen.blit(score_surf,score_rect)
+
+    snail_rect.x -= 4
+    if snail_rect.right <= 0: snail_rect.left = 800
+    screen.blit(snail_surf,snail_rect)
+    screen.blit(player_surf,snail_rect)
     pygame.display.update()
     clock.tick(60)
