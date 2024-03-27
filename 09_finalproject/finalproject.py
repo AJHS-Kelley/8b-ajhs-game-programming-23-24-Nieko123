3a9bb243d70b52be40c94772a26874396f1ea3bd
Nieko123 <115734414+Nieko123@users.noreply.github.com>
2024-03-27 14:46:52 -0400

    vo

--- 09_finalproject/finalproject.py
+++ 09_finalproject/finalproject.py
@@ -1,1 +1,25 @@
 # Final Project, Nieko Garnes, v0.0
+import sys, random, pygame
+
+resolution = 0 #0 = Low resolution (800, 600),1 = High Resilution (1920, 1080)
+
+if resolution == 0:
+    x = 800
+    y = 600
+else:
+    x = 1920
+    y = 1080
+
+pygame.init()
+
+difficulty = int(input("Please chose a difficulty, Enter 1 for Easy or 2 Hard.\n"))
+
+pygame.display.set_caption('NAME OF GAME -- EASY')
+pygame.display.set_caption('NAME OF GAME -- HARD')
+
+if difficulty = 1:
+    
+else:
+
+
+screen = pygame.display.set_mode((x, y))
