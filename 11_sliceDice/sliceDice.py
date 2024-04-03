13cbebe46138a9445842c40b0e7f79e3047bff23
Nieko123 <115734414+Nieko123@users.noreply.github.com>
2024-04-03 14:06:08 -0400

    v0.1

---
+++ 11_sliceDice/sliceDice.py
@@ -0,0 +1,24 @@
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+myString = "you are not real!\n"
+
+print(alphabet[0])
+print(alphabet[-1])
+
+# Slicing
+print(alphabet[0:4]) # Slice UP To second index.
+# alphabet[start:stop]
+
+# Slice from the Start
+print(alphabet[:7])
+
+# Slice to the End
+print(alphabet[12:])
+
+# Slice the whole %#$#! thing
+print(alphabet[:])
+
+# Negative Index Slicing
+print(alphabet[-4:-1])
+
+print(myString[0:18])
+
