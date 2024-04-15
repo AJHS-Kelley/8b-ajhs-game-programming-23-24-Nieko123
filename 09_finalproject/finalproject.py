60bf70fb86f954c82c59f9fa3fa8202172d89502
Nieko123 <115734414+Nieko123@users.noreply.github.com>
2024-04-15 13:53:06 -0400

    v0.3

--- 09_finalproject/finalproject.py
+++ 09_finalproject/finalproject.py
@@ -1,14 +1,7 @@
 # Final Project, Nieko Garnes, v0.0
-import sys, random, pygame
 
-
-# test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
-
-# game_name = test_font.render('Track game', False, (111, 196, 169))
-# game_name_rect = game_name.get_rect(center = (400, 80))
-
-# game_message = test_font.render('press Space to start', False, (111, 196, 169))
-# game_name_rect = game_message.get_rect(center = (400, 330))
+import pygame
+from pygame.locals import *
 
 resolution = 0 #0 = Low resolution (800, 600),1 = High Resilution (1920, 1080)
 
@@ -18,77 +11,122 @@ if resolution == 0:
 else:
     x = 1920
     y = 1080
-
 pygame.init()
-screen = pygame.display.set_mode((800,600))
-pygame.display.set_caption('Track')
 
-difficulty = int(input("Please chose a difficulty, Enter 1 for Easy or 2 Hard.\n"))
+# difficulty = int(input("Please chose a difficulty, Enter 1 for Easy or 2 Hard.\n"))
 
-# sky_surface = pygame.image.load('img/ultPie/sponge_sky.jpg').convert()
-ground_surface = pygame.image.load('img/ultPie/ground.png').convert()
 
-# Intro screen
-player_stand = pygame.image.load('img/ultPie/player_stand.png').convert_alpha()
-player_stand = pygame.transform.rotozoom(player_stand,0,2)
-player_stand_rect = player_stand.get_rect(center = (400,200))
+clock = pygame.time.Clock()
+fps = 60
 
-game_name = test_font.render('Pixel Runner',False,(111,196,169))
-game_name_rect = game_name.get_rect(center = (400,80))
+screen_width = 864
+screen_height = 936
 
-game_message = test_font.render('Press space to run',False,(111,196,169))
-game_message_rect = game_message.get_rect(center = (400,330))
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption('Flying man')
 
-# Timer 
-obstacle_timer = pygame.USEREVENT + 1
-pygame.time.set_timer(obstacle_timer,1500)
 
-while True:
+#define game variables
+ground_scroll = 0
+scroll_speed = 4
+flying = False
+game_over = False
+
+#load images
+bg = pygame.image.load('img/ultPie/background2.png')
+ground_img = pygame.image.load('img/ultPie/ground.png')
+
+
+class Bird(pygame.sprite.Sprite):
+	def __init__(self, x, y):
+		pygame.sprite.Sprite.__init__(self)
+		self.images = []
+		self.index = 0
+		self.counter = 0
+		for num in range(1, 4):
+			img = pygame.image.load(f'img/ultPie/flyman.png')
+			self.images.append(img)
+		self.image = self.images[self.index]
+		self.rect = self.image.get_rect()
+		self.rect.center = [x, y]
+		self.vel = 0
+		self.clicked = False
+
+	def update(self):
+
+		if flying == True:
+			#gravity
+			self.vel += 0.5
+			if self.vel > 8:
+				self.vel = 8
+			if self.rect.bottom < 768:
+				self.rect.y += int(self.vel)
+
+		if game_over == False:
+			#jump
+			if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
+				self.clicked = True
+				self.vel = -10
+			if pygame.mouse.get_pressed()[0] == 0:
+				self.clicked = False
+
+			#handle the animation
+			self.counter += 1
+			flap_cooldown = 5
+
+			if self.counter > flap_cooldown:
+				self.counter = 0
+				self.index += 1
+				if self.index >= len(self.images):
+					self.index = 0
+			self.image = self.images[self.index]
+
+			#rotate the bird
+			self.image = pygame.transform.rotate(self.images[self.index], self.vel * -2)
+		else:
+			self.image = pygame.transform.rotate(self.images[self.index], -90)
+
+
+bird_group = pygame.sprite.Group()
+
+flappy = Bird(100, int(screen_height / 2))
+
+bird_group.add(flappy)
+
+
+run = True
+while run:
+
+	clock.tick(fps)
+
+	#draw background
+	screen.blit(bg, (0,0))
+
+	bird_group.draw(screen)
+	bird_group.update()
+
+	#draw the ground
+	screen.blit(ground_img, (ground_scroll, 768))
+
+	#check if bird has hit the ground
+	if flappy.rect.bottom > 768:
+		game_over = True
+		flying = False
+
+
+	if game_over == False:
+		#draw and scroll the ground
+		ground_scroll -= scroll_speed
+		if abs(ground_scroll) > 35:
+			ground_scroll = 0
+
+
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
-			pygame.quit()
-			exit()
-
-		if game_active:
-			screen.blit(ground_surface,(0,300))
-
-		if event.type == obstacle_timer:
-			pass
-				# obstacle_group.add(Obstacle(choice(['fly','snail','snail','snail'])))
-		else:
-			if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
-				game_active = True
-				start_time = int(pygame.time.get_ticks() / 1000)
-
-
-	if game_active:
-		# screen.blit(sky_surface,(0,0))
-		screen.blit(ground_surface,(0,300))
-		# score = display_score()
-		
-		# player.draw(screen)
-		# player.update()
-
-		# obstacle_group.draw(screen)
-		# obstacle_group.update()
-
-		game_active = collision_sprite()
-		
-	else:
-		screen.fill((94,129,162))
-		screen.blit(player_stand,player_stand_rect)
-
-		score_message = test_font.render(f'Your score: {score}',False,(111,196,169))
-		score_message_rect = score_message.get_rect(center = (400,330))
-		screen.blit(game_name,game_name_rect)
-
-		if score == 0: screen.blit(game_message,game_message_rect)
-		else: screen.blit(score_message,score_message_rect)
+			run = False
+		if event.type == pygame.MOUSEBUTTONDOWN and flying == False and game_over == False:
+			flying = True
 
 	pygame.display.update()
-	# clock.tick(60)
 
-
-#Back Ground
-background_surface = pygame.image.load('img/ultPie/trackbackground.png').convert()
-
+pygame.quit()
